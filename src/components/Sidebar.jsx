f93e98bc665011e0ce5f4fcf4310daa9dd8167e2
Matheus Hisamoto <hisamoto.331@gmail.com>
2024-12-20 12:16:18 -0300

    area_atualizado

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -13,11 +13,7 @@ const Sidebar = () => {
       <SocialNetworkContainer />
       <InformationContainer />
       <a href="https://docs.google.com/document/d/1W_4lSdC6RdY81zhWeyWGZSGI3SDM9YT8/edit?usp=sharing&ouid=113676463100163427153&rtpof=true&sd=true" className="btn">
-        Currículo
-      </a>
-      <a href="www.linkedin.com/in/matheus-hisamoto-de-souza-4b6b14234" className="btn">
-        Linkedin
-      </a>
+        Currículo</a>
     </aside>
   );
 };
